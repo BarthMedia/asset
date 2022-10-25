97a2c99f5bc202d27ab6a87f91477a21e1b74313
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2022-10-25 23:33:05 +0200

    Update videoControls.js
    Popup functionality added

--- videoControls.js
+++ videoControls.js
@@ -5,20 +5,25 @@ const wrapperSelector = '[bmg_video = "wrapper"]'
 const embedSelector = '[bmg_video = "embed"]'
 const unmuteSelector = '[bmg_video = "unmute"]'
 const muteSelector = '[bmg_video = "mute"]'
+const popUpOpenerSelector = '[bmg_video = "popup-opener"]'
+const popUpCloserSelector = '[bmg_video = "popup-closer"]'
+const muteLevelSelector = 'bmg_video-mute-level'
 
 // Main function
 $(wrapperSelector).each(function()
 {
     // Local elements
-    let $video = $(this).find(embedSelector).find('video')
+    let $embed = $(this).find(embedSelector)
+    let $video = $embed.find('video')
     let $unmute = $(this).find(unmuteSelector)
     let $mute = $(this).find(muteSelector)
+    let $popUpOpener = $(this).find(popUpOpenerSelector)
 
     // Local functions
 
     // Play/pause
     let videoClicks = 0
-    $video.parent().click(function() 
+    $embed.click(function() 
     {
         if ( videoClicks == 0 )
         {
@@ -47,6 +52,27 @@ $(wrapperSelector).each(function()
         $mute.hide()
         $unmute.css({ 'display': 'flex' })
     })
-})
 
+    // Popup
+    console.log($popUpOpener)
+    if ( $popUpOpener )
+    {
+        // Popup elements & variables
+        let $popUpCloser = $(this).find(popUpCloserSelector)
+        let isSound = parseFloat( $embed.attr(muteLevelSelector) )
+
+        $popUpOpener.click(function()
+        {
+            $video[0].muted = isSound
+            $video[0].play()
+        })
+
+        $popUpCloser.click(function()
+        {
+            $video[0].muted = true
+            $video[0].play()
+        })
+    }
+})
+    
 })() /* End of: BMG video code */
