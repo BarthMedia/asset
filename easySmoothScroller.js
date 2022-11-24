6098b9eef1e9b78a0bad42055599fe8dd37f7254
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2022-11-24 17:52:05 +0100

    Create easySmoothScroller.js

---
+++ easySmoothScroller.js
@@ -0,0 +1,8 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+// create the smooth scroller FIRST!
+let smoother = ScrollSmoother.create({
+    content: '.main-wrapper',
+    smooth: 2,    // seconds it takes to "catch up" to native scroll position
+    effects: true // look for data-speed and data-lag attributes on elements and animate accordingly
+})
