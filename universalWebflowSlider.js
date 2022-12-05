1e4b12494282a814bc6a06280ad8a441c7342eb3
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2022-12-05 01:59:37 +0100

    Update universalWebflowSlider.js

--- universalWebflowSlider.js
+++ universalWebflowSlider.js
@@ -1,199 +1,351 @@
 (() => { /* Start of: BMG - Universal webflow slider code */
 
-// Global strings
+// + Global strings +
+
+// Selectors
 const sliderSelctor = '[bmg-uwsc = "Slider"]',
     maskSelector = '[bmg-uwsc = "Mask"]',
     leftSelector = '[bmg-uwsc = "Left Arrow"]',
     rightSelector = '[bmg-uwsc = "Right Arrow"]',
-    navSelector = '[bmg-uwsc = "Slide Nav"]'
+    navSelector = '[bmg-uwsc = "Slide Nav"]',
+    notASlideSelector = '[bmg-uwsc = "Not A Slide"]'
 
-// - Global styles -
+// Attributes
+const snappingAttribute = 'bmg-data-snapping',
+    snappingDelayAttribute = 'bmg-data-snapping-delay',
+    autoCreateDotsAttribute = 'bmg-data-auto-create-dots',
+    animationTimeAttribute = 'bmg-uwsc-animation-time',
+    cssShowAttribute = 'bmg-data-css-show',
+    cssHideAttribute = 'bmg-data-css-hide'
 
-// Insert stylesheet into <head> tag
-$('head').append(
-`
-<!-- Universal webflow slider styles -->
-<style>
-/* Add native CSS animation for smooth snapping when scrolling */
-${ maskSelector } {
-	scroll-snap-type: x mandatory
-}
-
-${ maskSelector } > * {
-	scroll-snap-align: center;
-}
-</style>
-`)
-
-
-// Main function
-$(sliderSelctor).each(function()
-{
-    // Local elements
-    let $slider = $(this),
-        $mask = $slider.find(maskSelector),
-        $left = $slider.find(leftSelector),
-        $right = $slider.find(rightSelector),
-        $nav = $slider.find(navSelector),
-        $dot = $nav.children().eq(1).clone(),
-        $currentDot = $nav.children().eq(0).css({ 'cursor': 'default' }).clone()
-
-    // Local variables
-    let nOfSlides = $mask.children().length,
-        animationTime = parseInt( $slider.attr( '[bmg-uwsc-animation-time]' ) || 350 ),
-        slideOuterWidthBoolean = $slider.attr( '[bmg-uwsc-slides-outer-width]' ) ? false : true,
-        cssHide =
-        {
-            'opacity': $slider.attr( '[bmg-uwsc-arrow-opacity]' ) || .5,
-            'transition-property': 'opacity',
-            'transition-duration': animationTime + 'ms',
-            'transition-timing-function': 'ease',
-            'cursor': 'default'
-        },
-        cssShow =
-        {
-            'opacity': 1,
-            'transition-property': 'opacity',
-            'transition-duration': animationTime + 'ms',
-            'transition-timing-function': 'ease',
-            'cursor': 'pointer'
-        },
-        thisSlideIsVisible = 0
-        
-
-    // - Local functions -
-
-    // Arrow functions
-    $left.css( cssHide )
-    if ( nOfSlides <= 1 ){ $right.css( cssHide ) }
-
-    $left.click(() => 
-    { // i = x < 0 ? 0 : ( x > nOfSlides -1 ? nOfSlides -1 : x  )
-        if ( thisSlideIsVisible - 1 >= 0 ) { thisSlideIsVisible-- }
-        scrollToItem( thisSlideIsVisible )
-    })
-
-    $right.click(() => 
-    {
-        if ( thisSlideIsVisible + 1 < nOfSlides ) { thisSlideIsVisible++ }
-        scrollToItem( thisSlideIsVisible )
-    })
-
-    // When slide i is visible do xyz
-    function thisSlideIsScrolledTo(i)
-    {
-        // Update local tracker variable
-        thisSlideIsVisible = i
-        
-        // Style dots
-        $dots.removeClass( currentDotClass ).addClass( dotClass ).css({ 'cursor': 'pointer' })
-        $dots.eq(i).removeClass( dotClass ).addClass( currentDotClass ).css({ 'cursor': 'default' })
-
-        // Style arrows
-        if ( i == 0 )
-        {
-            $left.css( cssHide )
-            $right.css( cssShow )
-        }
-        else if ( i == nOfSlides - 1 )
-        {
-            $left.css( cssShow )
-            $right.css( cssHide )
-        }
-        else
-        {
-            $left.css( cssShow )
-            $right.css( cssShow )
-        }
-    }
+// Defaults
+const snappingDefault = 'true',
+    snappingDelayDefault = 420,
+    animationTimeDefault = 350,
+    cssShowDefault = { opacity: 1, cursor: 'pointer' },
+    cssHideDefault = { opacity: 0.5, cursor: 'default' }
     
-    // On mask scroll do xyz
-    $mask.scroll(function()
+
+// + Main function +
+function main()
+{
+    $(sliderSelctor).each(function()
     {
-        if ( slideScrollLeftValueArray != 'Do not return.' )
+        // - - - Glocal Values - - -
+
+        // - Elements -
+        let $slider = $(this),
+            $mask = $slider.find(maskSelector),
+            $slides = $mask.children().not(notASlideSelector)
+            $left = $slider.find(leftSelector),
+            $right = $slider.find(rightSelector),
+            $nav = $slider.find(navSelector),
+            $dot = $nav.children().eq(1).clone(),
+            $currentDot = $nav.children().eq(0).css({ 'cursor': 'default' }).clone()
+
+        // - Attributes -
+        let isSnapping = $slider.attr(snappingAttribute) || snappingDefault,
+            snappingDelay = parseFloat( $slider.attr( snappingDelayAttribute ) || snappingDelayDefault ),
+            animationTime = parseFloat( $slider.attr( animationTimeAttribute ) || animationTimeDefault ),
+            cssShow = getJsonAttrVals( $slider, cssShowAttribute, { ...cssShowDefault } ),
+            cssHide = getJsonAttrVals( $slider, cssHideAttribute, { ...cssHideDefault } )
+
+        // Set duration if not declared
+        if (cssShow['duration'] == undefined) { cssShow['duration'] = animationTime / 1000 }
+        if (cssHide['duration'] == undefined) { cssHide['duration'] = animationTime / 1000 }
+
+        // - Fixed -
+        let dotClass = $dot.attr( 'class' ),
+            currentDotClass = $currentDot.attr( 'class' )
+
+        // - Variables -
+        let nOfSlides = $slides.length,
+            thisSlideIsCurrent = 0,
+            lastSrollableSlideIndex,
+            slideWidthElement,
+            paddingLeft,
+            paddingRight,
+            contentWidth,
+            gapValue
+
+
+        // - - - Styling - - - 
+        gsap.set( $left[0], { ...cssHide, duration: 0 } )
+        if ( nOfSlides <= 1 ) { gsap.set( $right[0], { ...cssHide, duration: 0 } ) }
+
+
+        // - - - Funcitons - - -
+
+        // - Create dots -
+        if ( $nav.attr( autoCreateDotsAttribute ) != 'false' )
         {
-            for ( let i = 0, n = nOfSlides; i < n; i++ )
+            $nav.empty()
+            $nav.append( $currentDot )
+
+            for ( let i = 1; i < nOfSlides; i++ )
             {
-                let x = $mask.scrollLeft() - slideScrollLeftValueArray[i]
-                
-                if ( x >= -1 && x <= 1 )
-                {
-                    thisSlideIsScrolledTo(i)
-                }
+                $nav.append( $dot.clone() )
             }
         }
+        let $dots = $nav.children()
+        
+
+        // - - Create calc values - on resize - -
+        function createCalcValues()
+        { 
+            // - Populate slide widths -
+            
+            // Reset
+            slideWidthElement = []
+
+            // Loop
+            $slides.each(function(index)
+            {
+                slideWidthElement.push({ single: $(this).outerWidth() })
+            })
+
+            // - Calculate content width -
+
+            // - Get padding values -
+            paddingLeft = parseFloat( $mask.css('padding-left') )
+            paddingRight = parseFloat( $mask.css('padding-right') )
+
+            // Reset
+            contentWidth = 0
+
+            // Loop
+            slideWidthElement.forEach(width => { contentWidth += width.single })
+
+            // - Get gap value -
+            gapValue = Math.round( ( $mask[0].scrollWidth - contentWidth - paddingLeft - paddingRight ) / ( nOfSlides - 1 ) )
+
+            // - Add joint value -
+            let tmpJointValue = 0
+
+            slideWidthElement.forEach((width, index) => 
+            {
+                if ( !index )
+                {
+                    width.joint = 0
+                }
+                else
+                {
+                    tmpJointValue+= width.single + gapValue
+                    width.joint = tmpJointValue
+                }
+            })
+
+            // - Update last scrollable dot -
+            let tmpScrollWidth = $mask[0].scrollWidth - $mask[0].clientWidth
+            
+            lastSrollableSlideIndex = returnSlideIndex( tmpScrollWidth, slideWidthElement )
+
+            $dots.each(function(index)
+            {
+                if ( index > lastSrollableSlideIndex )
+                {
+                    gsap.set( $(this)[0], { ...cssHide } )
+                }
+                else
+                {
+                    gsap.set( $(this)[0], { ...cssShow } )
+                }
+            })
+        }
+
+        // Call
+        createCalcValues()
+        $(window).resize(createCalcValues)
+
+        
+
+        // - - Scroll trigger - -
+        let snapTimeOutVarialbe
+        
+        $mask.scroll(function()
+        {
+            // - Update thisSlideIsCurrent -
+            thisSlideIsCurrent = returnSlideIndex( $mask.scrollLeft(), slideWidthElement )
+
+            // - Update buttons styles -
+            if ( thisSlideIsCurrent <= 0 )
+            {
+                gsap.to( $left[0], cssHide )
+            }
+            else if ( thisSlideIsCurrent >= lastSrollableSlideIndex )
+            {
+                gsap.to( $right[0], cssHide )
+            }
+            else
+            {
+                gsap.to( $left[0], cssShow )
+                gsap.to( $right[0], cssShow )
+            }
+
+            // - Update current dot -
+
+            // Style dots
+            $dots.removeClass( currentDotClass ).addClass( dotClass ).css({ 'cursor': 'pointer' })
+            $dots.eq( thisSlideIsCurrent ).removeClass( dotClass ).addClass( currentDotClass ).css({ 'cursor': 'default' })
+
+            // - Scroll snapping -
+            if ( isSnapping == 'true' )
+            {
+                // Clear
+                clearTimeout(snapTimeOutVarialbe)
+
+                // Set
+                if ( thisSlideIsCurrent < lastSrollableSlideIndex ) 
+                {
+                    snapTimeOutVarialbe = setTimeout(() => 
+                    {
+                        scrollToItem( thisSlideIsCurrent )
+                    }, snappingDelay)
+                }
+            }
+        })
+
+
+        // - - Click events - -
+
+        // - Buttons -
+        $left.click(() => 
+        {
+            if ( thisSlideIsCurrent > 0 ) { thisSlideIsCurrent-- }
+            scrollToItem( thisSlideIsCurrent )
+        })
+
+        $right.click(() => 
+        {
+            if ( thisSlideIsCurrent < lastSrollableSlideIndex ) { thisSlideIsCurrent++ }
+            scrollToItem( thisSlideIsCurrent )
+        })
+
+        // - dots -
+        $dots.each(function(index)
+        {
+            $(this).click(() => 
+            {
+                scrollToItem( index )
+            })
+        })
+
+
+        // - - Scroll to item x - -
+        function scrollToItem( x )
+        {
+            // Prevent double snapping
+            clearTimeout(snapTimeOutVarialbe)
+            
+            // Animate
+            $mask.stop().animate( { scrollLeft: slideWidthElement[x].joint }, animationTime )
+        }
+    })
+}
+
+
+// + Helper functions +
+
+// - - - Return closest slide index - - -
+function returnSlideIndex( scrollPosition, slideWidthElement )
+{
+    // Value
+    let smallestValue,
+        closestSlide
+
+    // Loop
+    slideWidthElement.forEach((width, index) => 
+    {
+        // Logic
+        if ( smallestValue == undefined ) // Case 0
+        {
+            smallestValue = Math.abs( width.joint - scrollPosition )
+            closestSlide = index
+        }
         else
         {
-            populateSlideScrollLeftValueArray()
+            // Values 
+            let cmpValue = Math.abs( width.joint - scrollPosition )
+
+            // Logic
+            if ( cmpValue < smallestValue )
+            {
+                smallestValue = cmpValue
+                closestSlide = index
+            }
         }
     })
 
-    // Create dots
-    if ( $nav.attr( 'bmg-uwsc-auto-dots' ) != 'off' )
+    return closestSlide
+}
+
+
+// - - - String related functions - - -
+
+// - Get attribute values -
+function getJsonAttrVals( $element, attribute, defaultVals, objectMode = true )
+{
+    return ($element.attr(attribute) || '{}') == '{}' ?
+    defaultVals
+    : JSON.parse( preJsonParse( $element.attr(attribute), true ) )
+}
+
+// - Prepare for JSON parse -
+function preJsonParse( string, objectMode = true )
+{
+    let array = trimBothStringSides( string.replace(', ', ',') ).split(','),
+        newString = '',
+        arrayLength = array.length - 1
+
+    array.forEach((item, i) => 
     {
-        $nav.empty()
-        $nav.append( $currentDot )
-
-        for ( let i = 1; i < nOfSlides; i++ )
-        {
-            $nav.append( $dot.clone() )
-        }
-    }
-
-    // Add click events to dots
-    let $dots = $nav.children(),
-        dotClass = $dot.attr( 'class' ),
-        currentDotClass = $currentDot.attr( 'class' )
+        item
+            .replace(/\'/g, '')
+            .replace(': ', ':')
+            .split(':')
+            .forEach((item, i2) => 
+            {
+                newString += `"${ item }"${ i2 > 0 ? '' : ': '}`
+            })
         
-    $dots.each(function(index)
-    {
-        $(this).click(() => 
-        {
-            scrollToItem( index )
-        })
+        newString += i < arrayLength ? ', ' : ''
     })
 
-    // Save indivual slide scrollLeft() values
-    let slideScrollLeftValueArray = 'Do not return.',
-        widthOfSlides
-
-    // Allow new value calculation
-    $( window ).resize(() => { slideScrollLeftValueArray = 'Do not return.' })
-
-    function populateSlideScrollLeftValueArray()
+    if ( objectMode )
     {
-        // Reduce necessary processing power
-        if ( slideScrollLeftValueArray != 'Do not return.' ) { return }
-        
-        // Reset
-        slideScrollLeftValueArray = [0]
-        widthOfSlides = 0
-
-        // Populate base values
-        $mask.children().each(function()
-        {
-            widthOfSlides += $(this).outerWidth( slideOuterWidthBoolean )
-            slideScrollLeftValueArray.push( widthOfSlides )
-        })
-
-        // Finetune values
-        let gap = Math.round( ( $mask[0].scrollWidth - widthOfSlides ) / ( nOfSlides - 1 ) )
-        
-        for ( let i = 1, n = nOfSlides; i < n; i++ )
-        {
-            slideScrollLeftValueArray[i] += gap * i
-        }
+        return `{${ newString }}`
     }
-
-    // Scroll to item x
-    function scrollToItem( x )
+    else
     {
-        populateSlideScrollLeftValueArray()
-        let n = slideScrollLeftValueArray[x]
-        
-        $mask.css("scroll-snap-type", "none")
-        $mask.stop().animate( { scrollLeft: n }, animationTime, () => $mask.css("scroll-snap-type", "x mandatory") )
+        return newString
     }
-})
+}
+
+// Removes two both string sides
+function trimBothStringSides( string )
+{
+    return string.substring(1).slice(0, -1)
+}
+
+
+// - - - Load related functions - - -
+
+// Allows for loading other scripts
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'script',
+        success: callback,
+        async: true
+    })
+}
+
+// Loader
+"undefined" == typeof gsap
+    ? $.loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js", function () {
+          main()
+      })
+    : main()
+
 
 })() /* Start of: BMG - Universal webflow slider code */
